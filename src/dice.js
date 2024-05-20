b1fd6819364b68b62ef2dec21b3e23c8c1c685fb
flo629 <144320013+flo629@users.noreply.github.com>
2024-05-20 11:57:23 +0200

    5. green Test by Florian Korcian

--- src/dice.js
+++ src/dice.js
@@ -2,6 +2,11 @@
 function Dice() {}
 Dice.prototype.findCorrectNumber = function(dice_numbers) {
 	
+	if(dice_numbers.length < 4)
+	{
+		return 0;
+	}
+	
 	var electrons = 0;
 	electrons += dice_numbers.filter(x => x === 3).length * 2;
 	electrons += dice_numbers.filter(x => x === 5).length * 4;
