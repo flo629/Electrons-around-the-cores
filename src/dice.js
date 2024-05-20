3dc975e4618012006c76f5af69ab81e16664179a
flo629 <144320013+flo629@users.noreply.github.com>
2024-05-20 11:46:13 +0200

    third green test by Florian Korcian

--- src/dice.js
+++ src/dice.js
@@ -4,8 +4,11 @@ Dice.prototype.findCorrectNumber = function(dice_numbers) {
 	
 	if (dice_numbers[0] === 1) {
 		return 6;
-	  } else {
+	  } else if(dice_numbers[0] === 2) {
 		return 4;
+	  }else
+	  {
+		return 2;
 	  }
 
 	
