479d3fff335e372423cc3ab9728b3372aa4f9bf3
flo629 <144320013+flo629@users.noreply.github.com>
2024-05-20 11:54:41 +0200

    fourth red and green Test by Florian Korcian

--- spec/dice_spec.js
+++ spec/dice_spec.js
@@ -14,6 +14,13 @@ describe('dice', function() {
 	it("should return 2 for [6, 6, 4, 4, 1, 3]", function() {
         expect(dice.findCorrectNumber([6, 6,4, 4, 1, 3])).toEqual(2);
     });
+
+	it("should return 12 for [ 3, 5, 3, 5, 4, 2 ]", function() {
+        expect(dice.findCorrectNumber([ 3, 5, 3, 5, 4, 2 ])).toEqual(12);
+    });
 	
+
+	
+
 
 });
